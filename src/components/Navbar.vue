ac82f977925da1223fe61f6b5c29b46bbe090728
Isis Lázaro <isislazaro@gmail.com>
2019-10-21 17:21:01 -0500

    navbar burger activation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,60 @@
 <template>
   <nav class="navbar">
-    <router-link :to="{ name: 'home' }">
-      <h1 class="title">Isis Lázaro</h1>
-      <h2 class="subtitle is-6">desarrolladora front-end</h2>
-    </router-link>
-    <div class="navbar-end">
-      <router-link class="navbar-item" :to="{ name: 'proyectos' }"
-        >Proyectos</router-link
+    <div class="navbar-brand">
+      <router-link :to="{ name: 'home' }" id="logoLink" class="navbar-item">
+        <h1 class="title">Isis Lázaro</h1>
+        <h2 class="subtitle is-6">desarrolladora front-end</h2>
+      </router-link>
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navMenu"
+        @click="showNav = !showNav"
+        :class="{ 'is-active': showNav }"
       >
-      <router-link class="navbar-item" :to="{ name: 'habilidades' }"
-        >Herramientas</router-link
-      >
-      <router-link class="navbar-item" :to="{ name: 'about' }"
-        >Acerca de mí</router-link
-      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navMenu" class="navbar-menu" :class="{ 'is-active': showNav }">
+      <div class="navbar-end">
+        <router-link class="navbar-item" :to="{ name: 'proyectos' }"
+          >Proyectos</router-link
+        >
+        <router-link class="navbar-item" :to="{ name: 'habilidades' }"
+          >Herramientas</router-link
+        >
+        <router-link class="navbar-item" :to="{ name: 'about' }"
+          >Acerca de mí</router-link
+        >
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data() {
+    return {
+      showNav: false
+    };
+  }
 };
 </script>
 
 <style scoped>
 .navbar {
   background: #cc1994;
-  margin: 0 3em;
+  /* margin: 0 3em; */
 }
+#logoLink {
+  display: block;
+}
+
 .title {
   font-family: "Fugaz One", cursive;
   letter-spacing: 0.1rem;
