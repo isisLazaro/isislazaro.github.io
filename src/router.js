ca7fcfcb4e359d940176cce66166f11673964696
Isis Lázaro <isislazaro@gmail.com>
2019-11-13 07:48:11 -0600

    history mode activated and error page created

--- src/router.js
+++ src/router.js
@@ -4,10 +4,11 @@ import Home from "./views/Home.vue";
 import Projects from "./views/Projects.vue";
 import Tools from "./views/Tools.vue";
 import About from "./views/About.vue";
-
+import NotFoundComponent from "./views/FileNotFound.vue";
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -28,6 +29,7 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
-    }
+    },
+    { path: "*", name: "notfound", component: NotFoundComponent }
   ]
 });
