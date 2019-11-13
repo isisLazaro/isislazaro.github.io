2bedce6a2e5479089ae7c076e4df90c5bac600b7
Isis Lázaro <isislazaro@gmail.com>
2019-11-13 07:57:39 -0600

    delete about route

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Projects from "./views/Projects.vue";
 import Tools from "./views/Tools.vue";
-import About from "./views/About.vue";
 import NotFoundComponent from "./views/FileNotFound.vue";
 Vue.use(Router);
 
@@ -24,11 +23,6 @@ export default new Router({
       path: "/tools",
       name: "herramientas",
       component: Tools
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About
     },
     { path: "*", name: "notfound", component: NotFoundComponent }
   ]
